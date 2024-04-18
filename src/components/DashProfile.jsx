0e84a84697a6b649c3cc175f75977aa226708f6f
Baovola <baovolarav@gmail.com>
2024-04-18 19:35:12 +0200

    add firebase functionnality to image

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,11 +1,16 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
 import { useSelector} from "react-redux";
 import {useEffect, useRef, useState} from "react";
+import {getStorage, uploadBytesResumable,ref, getDownloadURL} from 'firebase/storage';
+import { app } from "../firebase";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const[imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] =useState(null);
+  //console.log( imageFileUploadProgress,imageFileUploadError);
   const filePickerRef = useRef();
 
   const handelImageChange =(e)=>{
@@ -27,7 +32,37 @@ export default function DashProfile() {
 
   const uploadImage = async () =>{
     //console.log("uploading image...");
-  }
+    //service firebase.storage {
+    //  match /b/{bucket}/o {
+    //    match /{allPaths=**} {
+    //      allow read;
+    //      allow write: if
+    //      request.resource.size < 2 * 1024 * 1024 &&
+    //      request.resource.contentType.matches('image/.*')
+    //    }
+
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref (storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot)=>{
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error)=>{
+          setImageFileUploadError('Could not upload image(File must be less than 2MB)');
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+          setImageFileUrl(downloadURL);
+        });
+      }
+
+    );
+      };
+  
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
@@ -43,6 +78,12 @@ export default function DashProfile() {
          border-8 border-[lightgray]"
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color='failure'>
+          {imageFileUploadError}
+        </Alert>
+        )}
+        
         <TextInput
           type="text"
           id="username"
